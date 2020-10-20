0f067697f9c32d15d54fe94805eea36cef113b71
Sol <solmseda@gmail.com>
2020-10-19 23:28:22 -0300

    Todos os testes OK com o Mockup

--- Testes.py
+++ Testes.py
@@ -76,96 +76,82 @@ class Testmock (unittest.TestCase):
     def test_07_inserir_jogador_ok_condicao_retorno(self):
         m = mock.Mock()
         print("Caso de teste 7 - Inserir com sucesso")
-        m.jogador_insere().return_value = 0
         m.jogador_insere.return_value = 0
-        self.assertEqual(m.jogador_insere("Mario", "azul"), 0)
-
-    def test_08_verifica_jogador_inserido_com_sucesso(self):            #DANDO ERRO
-        m = mock.Mock()
-        m.lista_jogadores.nome = 'Mario'
-        m.lista_jogadores.cor = 'azul'
-        print("Caso de teste 8 - verifica insercão")
-        self.assertIn({'nome': 'Mario', 'cor': 'azul'}, m.lista_jogadores())
-
-    def test_09_inserir_jogador_ja_existente(self):            #DANDO ERRO
-        m = mock.Mock()
-        print("Caso de teste 9 - inserir jogador ja existente")
-        retorno_esperado = m.jogador_insere("Mario", "Azul")
-        self.assertEqual(retorno_esperado, 1)
+        self.assertEqual(m.jogador_insere("Mario", "Azul"), 0)
 
 
     # Abrir a tela de regras
-    def test_10_abrir_regras_ok(self):
+    def test_08_abrir_regras_ok(self):
         m = mock.Mock()
-        print("Caso de teste 10 - Abrir a tela de regras")
+        print("Caso de teste 08 - Abrir a tela de regras")
         m.abre_regras.return_value = 0
         self.assertEqual(m.abre_regras(), 0)
 
-    def test_11_abrir_regras_falha(self):
+    def test_09_abrir_regras_falha(self):
         m = mock.Mock()
-        print("Caso de teste 11 - Abrir tela de Regras falhou")
+        print("Caso de teste 09 - Abrir tela de Regras falhou")
         m.abre_regras.return_value = 1
         self.assertEqual(m.abre_regras(), 1)
 
 
     # Abrir o histórico
-    def test_12_abrir_historico_ok(self):
+    def test_10_abrir_historico_ok(self):
         m = mock.Mock()
-        print("Caso de teste 12 - Abrir a tela de histórico")
+        print("Caso de teste 10 - Abrir a tela de histórico")
         m.abre_historico.return_value = 0
         self.assertEqual(m.abre_historico(), 0)
 
-    def test_13_abrir_historico_falha(self):
+    def test_11_abrir_historico_falha(self):
         m = mock.Mock()
-        print("Caso de teste 13 - Abrir a tela de histórico falhou")
+        print("Caso de teste 11 - Abrir a tela de histórico falhou")
         m.abre_historico.return_value = 1
         self.assertEqual(m.abre_historico(), 1)
 
 
     # iniciar a partida
-    def test_14_iniciar_partida_ok(self):
+    def test_12_iniciar_partida_ok(self):
         m = mock.Mock()
-        print("Caso de teste 14 - Iniciar a partida")
+        print("Caso de teste 12 - Iniciar a partida")
         m.iniciar_partida.return_value = 0
         self.assertEqual(m.iniciar_partida(), 0)
 
-    def test_15_iniciar_partida_falha(self):
+    def test_13_iniciar_partida_falha(self):
         m = mock.Mock()
-        print("Caso de teste 15 - Iniciar a partida falhou")
+        print("Caso de teste 13 - Iniciar a partida falhou")
         m.iniciar_partida.return_value = 1
         self.assertEqual(m.iniciar_partida(), 1)
 
 
     # teste de uma jogada
-    def test_16_realizar_jogada_ok(self):
+    def test_14_realizar_jogada_ok(self):
         m = mock.Mock()
-        print("Caso de teste 16 - realizar uma jogada")
-        retorno_esperado = m.movimenta(m.posicao(), m.novas_posicoes())
-        self.assertEqual(retorno_esperado, 0)
+        print("Caso de teste 14 - realizar uma jogada")
+        m.movimenta.return_value = 0
+        self.assertEqual(m.movimenta(m.posicao(), m.novas_posicoes()), 0)
 
-    def test_17_realizar_jogada_falha(self):
+    def test_15_realizar_jogada_falha(self):
         m = mock.Mock()
-        print("Caso de teste 17- realizar uma jogada falhou")
-        retorno_esperado = m.movimenta(m.posicao(), m.novas_posicoes())
-        self.assertEqual(retorno_esperado, 1)
-
+        print("Caso de teste 15 - realizar uma jogada falhou")
+        m.movimenta.return_value = 1
+        self.assertEqual(m.movimenta(m.posicao(), m.novas_posicoes()), 1)
+    
     #------------partida.py------------#
     #exibir vencedor
-    def test_18_exibir_vencedor_ok(self):
+    def test_16_exibir_vencedor_ok(self):
         m = mock.Mock()
-        print("Caso de teste 18 - exibir o vencedor de uma partida")
+        print("Caso de teste 16 - exibir o vencedor de uma partida")
         m.exibe_vencedor.return_value = 0
         self.assertEqual(m.exibe_vencedor(), 0)
 
-    def test_19_exibir_vencedor_falha(self):
+    def test_17_exibir_vencedor_falha(self):
         m = mock.Mock()
-        print("Caso de teste 19 - exibir o vencedor de uma partida")
+        print("Caso de teste 17 - exibir o vencedor de uma partida")
         m.exibe_vencedor.return_value = 1
         self.assertEqual(m.exibe_vencedor(), 1)
 
-    def test_20_abre_menu_ok(self):
+    def test_18_abre_menu_ok(self):
         m = mock.Mock()
-        print("Caso de teste 20 - abrir o menu após o final de uma partida")
+        print("Caso de teste 18 - abrir o menu após o final de uma partida")
         m.abre_menu.return_value = 0
         self.assertEqual(m.abre_menu(), 0)
 
