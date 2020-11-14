a9aa7ab0e2d41328ef287be0adf278b6428a8b03
Victornrib <victornrib@gmail.com>
2020-11-14 14:13:51 -0300

    oioi

---
+++ jogada.py
@@ -0,0 +1,27 @@
+# Autores: Eduardo Sardenberg e Victor Nielsen (1711600 e 1811545)
+# Horas Trabalhadas ao todo: 8 horas e meia
+"""	
+Horas Trabalhadas - Sol Castilho de Moraes Sêda
+11/11/2020 -> INICIO: 13:20
+"""
+
+import random
+import jogadorPeca
+
+def rolaDado():
+    return random.randint(1, 6)
+
+def validaJogada(jogadores, posicoes, novasPosicoes, valida)
+
+
+
+def checaTorre()
+
+def movimentaPeca(posicoes, novasPosicoes):
+    valida = false
+    validaJogada(posicoes, novasPosicoes, valida)
+    if(valida == true):
+        posicoes = novasPosicoes
+        return 0
+    else:
+        return 1
