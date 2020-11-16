e7e78fd1ab7a332540b68694ae3975717b99f064
eduardosardenberg <eduardo.sardenberg99@gmail.com>
2020-11-15 23:29:30 -0300

    torre

--- jogada.py
+++ jogada.py
@@ -12,19 +12,77 @@ import jogadorPeca
 def rolaDado(num):
     num = random.randint(1, 6)
 
-def bool checaTorre(jogador, posicao):
+def checaTorre(jogador, posicao, outroPeao):
     if (jogador[2][0] == posicao):
+        outroPeao == jogador [2][0]
         return True
     else if(jogador[2][1] == posicao):
+        outroPeao == jogador [2][1]
         return True
     else if(jogador[2][2] == posicao):
+        outroPeao == jogador [2][2]
         return True
     else if(jogador[2][3] == posicao):
+        outroPeao == jogador [2][3]
         return True
-    return false
+    return False
 
+def validaJogada(Jogador, posicaoAtual, dado, novaPosicao, valida):
 
-def validaJogada(jogadores, posicaoAtual, dado, novaPosicao, valida)
+    #SAIDA DA BASE
+    p1 = 76
+    p2 = 80
+    p3 = 84
+    p4 = 88
+    for i in range(0, 3):
+        if(posicaoAtual == jogadorPeca.posicaoPecas(Jogador, i)):
+            if(Jogador == "Verde"):
+                if(Jogador[2][i] == p1++):
+                    if(dado == 6):
+                        valida = True
+            else if(Jogador == "Vermelho"):
+                if(Jogador[2][i] == p2++):
+                    if(dado == 6):
+                        valida = True
+            else if (Jogador == "Azul"):
+                if(Jogador[2][i] == p3++):
+                    if(dado == 6):
+                        valida = True
+            else if (Jogador == "Amarelo"):
+                if(Jogador[2][i] == p4++):
+                    if(dado == 6):
+                        valida = True
+
+    #VERIFICANDO A MOVIMENTACAO DA PECA
+    if ((novaPosicao - posicaoAtual) <= 0) or ((novaPosicao - posicaoAtual) > 6):
+        valida = False 
+        return 1
+    
+    #VERIFICAR PECA COME TORRE (IMPOSSIVEL DE ACONTECER)
+    outra
+    torre = checaTorre(Jogador_1, novaPosicao, outra)
+    if (torre == True):
+        valida = False
+        return 1
+
+    torre = checaTorre(Jogador_2, novaPosicao, outra)
+    if (torre == True):
+        valida = False
+        return 1
+        
+    torre = checaTorre(Jogador_3, novaPosicao, outra)
+    if (torre == True):
+        valida = False
+        return 1
+
+    torre = checaTorre(Jogador_4, novaPosicao, outra)
+    if (torre == True):
+        valida = False
+        return 1
+    
+
+    valida = True
+    return 0
 
 
 def movimentaPeca(jogadorAtual, posicaoAtual, novaPosicao):
@@ -41,18 +99,18 @@ def movimentaPeca(jogadorAtual, posicaoAtual, novaPosicao):
             p4 = 88
             for i in range(0, 3):
                 if(novaPosicao == jogadorPeca.posicaoPecas(Jogador_2, i)):
-                if(Jogador_2 == "Verde"):
-                    Jogador_2[2][i] = p1++
-                    posicaoAtual = novaPosicao
-                else if(Jogador_2 == "Vermelho"):
-                    Jogador_2[2][i] = p2++
-                    posicaoAtual = novaPosicao
-                else if (Jogador_2 == "Azul"):
-                    Jogador_2[2][i] = p3++
-                    posicaoAtual = novaPosicao
-                else if (Jogador_2 == "Amarelo"):
-                    Jogador_2[2][i] = p4++
-                    posicaoAtual = novaPosicao
+                    if(Jogador_2 == "Verde"):
+                        Jogador_2[2][i] = p1++
+                        posicaoAtual = novaPosicao
+                    else if(Jogador_2 == "Vermelho"):
+                        Jogador_2[2][i] = p2++
+                        posicaoAtual = novaPosicao
+                    else if (Jogador_2 == "Azul"):
+                        Jogador_2[2][i] = p3++
+                        posicaoAtual = novaPosicao
+                    else if (Jogador_2 == "Amarelo"):
+                        Jogador_2[2][i] = p4++
+                        posicaoAtual = novaPosicao
 
             p1 = 76
             p2 = 80
